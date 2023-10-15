bdc059d53e68ef727ba740d48ad39d780ed54788
kweku7k <mr.adumatta@gmail.com>
2023-10-15 12:42:44 +0000

    pending labels

--- budget.py
+++ budget.py
@@ -1,3 +1,4 @@
+import math
 class Category:
     def __init__(self, name):
         self.ledger = []
@@ -91,6 +92,58 @@ class Category:
 
 
 
+def getPercentatges(categories):
+    array = []
+    
+    totalWithdrawals = 0
+    for category in categories:
+        withdrawals = 0
+        deposits = 0
+        # withdrawals/get_balance * 100?
+        for l in category.ledger:
+            if l['amount'] < 0:
+                withdrawals += l['amount']
+            else:
+                deposits += l['amount']
+        
+        totalWithdrawals += withdrawals
+        array.append({'name':category.name,'withdrawals':withdrawals})
+        # withdrawals from each category over total withdrawals * 100 to get percentage
+
+    for category in array:
+        percentage = (abs(category['withdrawals']) / abs(totalWithdrawals))*100
+        category['percentage'] = percentage
+
+    return array
+
+def rightAlign(x):
+    maxLength = 3
+    length = len(str(x))
+    difference = maxLength - length
+    rightAlignedValue = (' '*difference) + str(x)
+    return rightAlignedValue
+
 
 def create_spend_chart(categories):
-    pass
+    percentages = getPercentatges(categories)
+    # print the values as a bar chart
+    barChart = f''
+    barChart += 'Percentage spent by category\n'
+    for x in reversed(range(0, 110, 10)):
+        label = rightAlign(x)
+        barChart+=label+'| '
+        for p in percentages: #This should preserve the order
+            value = int(round(p['percentage'], -1))
+            value = math.floor(p['percentage'] / 10) * 10
+            if value == x or p.get('active'): #We might have to strip some values
+                barChart += 'o  '
+                p['active'] = True
+            else:
+                barChart += '   '
+        barChart +='\n'
+        # function to check if the category is in that range
+
+
+    print(barChart)
+    return barChart
+    
